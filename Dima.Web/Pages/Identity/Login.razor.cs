0088c08ea739174ea0da4fc57126f0c41b34ec48
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-03-31 21:10:36 -0300

    Implementando code-behind para páginas de login e registro e logout.

---
+++ Dima.Web/Pages/Identity/Login.razor.cs
@@ -0,0 +1,84 @@
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Web.Security;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Identity;
+
+public partial class LoginPage : ComponentBase
+{
+    #region Dependencies 
+    
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+    
+    [Inject]
+    public IAccountHandler Handler { get; set; } = null!;
+    
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+    
+    [Inject]
+    public ICookieAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+    
+    #endregion
+    
+    #region Properties
+    
+    public bool IsBusy { get; set; } = false;
+    public LoginRequest IputModel { get; set; } = new();
+    
+    #endregion
+    
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        Console.WriteLine($"Usuário autenticado? {user.Identity?.IsAuthenticated}");
+
+        if (user.Identity is { IsAuthenticated: true })
+        {
+            Console.WriteLine("Redirecionando para home...");
+            NavigationManager.NavigateTo("/");
+        }
+    }
+
+    #endregion
+
+    #region Methods
+    
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var result = await Handler.LoginAsync(IputModel);
+
+            if (result.IsSuccess)
+            {
+                await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                AuthenticationStateProvider.NotifyAuthenticationStateChanged();
+                NavigationManager.NavigateTo("/");
+            }else 
+            {
+                Snackbar.Add(result.Message, Severity.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+    
+    #endregion
+}
