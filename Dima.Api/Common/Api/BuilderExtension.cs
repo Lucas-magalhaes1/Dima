3c5a5dfaddc8b79ea9ee0bd2884d1a915a62f8c1
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-04-13 18:18:11 -0300

    Builder Extension

--- Dima.Api/Common/Api/BuilderExtension.cs
+++ Dima.Api/Common/Api/BuilderExtension.cs
@@ -62,5 +62,6 @@ public static class BuilderExtension
     {
         builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
         builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+        builder.Services.AddTransient<IReportHandler, ReportHandler>();
     }
 }
